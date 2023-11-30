f7543fb42d78a29e962afede4f0bd5942adf52e4
KevinCarm <kevincarm016@hotmail.com>
2023-11-30 12:41:44 -0600

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
+import About from "./components/About/About.jsx";
+import Experience from "./components/Experience/Experience.jsx";
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <About />
+    <Experience />
     </>
   )
 }
